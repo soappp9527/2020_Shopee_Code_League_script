1dbed07d6ff7f27476a6dc43bf4a58ab651e9aa4
soappp9527 <soappp9527@gmail.com>
2020-07-17 20:24:41 +0800

    week5 optimized

--- week5/week5.py
+++ week5/week5.py
@@ -2,28 +2,34 @@ import numpy as np
 import pandas as pd
 
 data = pd.read_csv("../input/open-shopee-code-league-logistic/delivery_orders_march.csv")
-
 #split address to location
-data["buy"] = data["buyeraddress"].str.lower().str.split(" ").str[-1]
-data["sell"] = data["selleraddress"].str.lower().str.split(" ").str[-1]
+data["buy"] = data["buyeraddress"].apply(lambda x: x.split(" ")[-1].lower())
+data["sell"] = data["selleraddress"].apply(lambda x: x.split(" ")[-1].lower())
 
 #count SLA day
-def countday(df):
-    if df["buy"] == "manila" and df["sell"] == "manila":
-        return 3
-    elif df["buy"] == "manila" and df["sell"] == "luzon":
-        return 5
-    elif df["buy"] == "luzon" and df["sell"] == "manila":
-        return 5
-    elif df["buy"] == "luzon" and df["sell"] == "luzon":
-        return 5
-    else:
-        return 7
+SLA = pd.DataFrame(
+    data = [
+            ["manila","manila",3],
+            ["manila","luzon",5],
+            ["manila","visayas",7],
+            ["manila","mindanao",7],
+            ["luzon","manila",5],
+            ["luzon","luzon",5],
+            ["luzon","visayas",7],
+            ["luzon","mindanao",7],
+            ["visayas","manila",7],
+            ["visayas","luzon",7],
+            ["visayas","visayas",7],
+            ["visayas","mindanao",7],
+            ["mindanao","manila",7],
+            ["mindanao","luzon",7],
+            ["mindanao","visayas",7],
+            ["mindanao","mindanao",7]
+           ],
+    columns=["buy", "sell", "SLA"]
+)
 
-data["SLA"] = data.apply(countday, axis =1)
-
-test = data.head(20)
-test["2nd_deliver_attempt"].fillna(0, inplace = True)
+data = pd.merge(data, SLA, on = ["buy", "sell"])
 
 #GMT+8
 data[["pick", "1st_deliver_attempt", "2nd_deliver_attempt"]] += 8*60*60
@@ -39,16 +45,9 @@ holidays = ["2020-03-08", "2020-03-25", "2020-03-30", "2020-03-31"]
 data["frist_try"] = np.busday_count(data["pick"], data["1st_deliver_attempt"], weekmask="1111110", holidays = holidays)
 data["second_try"] = np.busday_count(data["1st_deliver_attempt"], data["2nd_deliver_attempt"], weekmask="1111110", holidays = holidays)
 
-def is_late(df):
-    if df["frist_try"] > df["SLA"]:
-        return 1
-    else:
-        if df["second_try"] > 3:
-            return 1
-        else:
-            return 0        
+#judge
+data["is_late"] = (data["frist_try"] > data["SLA"]) | (data["second_try"] > 3)
+data["is_late"] = data["is_late"].astype(int)
 
-data["is_late"] = data.apply(is_late, axis =1)
-
-submission = data[["orderid", "is_late"]]
+submission = data[["orderid","is_late"]]
 submission.to_csv("submission.csv", index = False)
